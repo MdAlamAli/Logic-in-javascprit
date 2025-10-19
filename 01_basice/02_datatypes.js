011858a29fceef9d4851eb1ba8caa9256e90ca16
MdAlamAli <aa4432678@gmail.com>
2025-10-19 11:11:37 +0000

    datatypes

---
+++ 01_basice/02_datatypes.js
@@ -0,0 +1,15 @@
+"use Strict" //treat all js code newer verison
+
+let name="Harish"
+let age=22
+let isLoggedIn=true
+
+
+// string => ""
+// number => 23
+// booleam => true/false
+// null => standalone value
+// undefine => undefine
+// symbol=> unique
+
+
